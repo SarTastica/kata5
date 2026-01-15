0491334e392101ea7de2d54b5c094278d06172a3
SarTastica <sartastica@gmail.com>
2026-01-15 17:50:56 +0000

    JFreeChartHistogramDisplay class created

---
+++ src/main/java/software/ulpgc/kata4/view/JFreeChartHistogramDisplay.java
@@ -0,0 +1,48 @@
+package software.ulpgc.kata4.view;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import software.ulpgc.kata4.viewmodel.Histogram;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JFreeChartHistogramDisplay extends JFrame implements HistogramDisplay {
+    private final Histogram<String> histogram;
+
+    public JFreeChartHistogramDisplay(Histogram<String> histogram) {
+        this.histogram = histogram;
+        setTitle("Histogram Display");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        add(createPanel());
+    }
+
+    private JPanel createPanel() {
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        for (String key : histogram.keys()){
+            dataset.addValue(histogram.get(key), "Frecuencia", key);
+        }
+
+        JFreeChart chart = ChartFactory.createBarChart(
+                "Histograma de tipos",
+                "Categoria",
+                "N. de titulos",
+                dataset,
+                PlotOrientation.VERTICAL,
+                false,
+                false,
+                false
+        );
+        return new ChartPanel(chart);
+    }
+
+    @Override
+    public void show(){
+        this.setVisible(true);
+    }
+}
